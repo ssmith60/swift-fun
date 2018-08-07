c6b4f43fbac5727629beba6bd5e1a91c6a8d77f8
stuart smith <stuartsmith@stuarts-macbook-pro.local>
2018-08-06 21:23:03 -0700

    first commit

--- SwiftFun/ViewController.swift
+++ SwiftFun/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func changeText(_ sender: Any) {
+        buttonCount = buttonCount + 1
+        print(buttonCount)
+        if buttonCount >= 10{
+                    view.backgroundColor = UIColor.blue
+            myLabel.text = "That makes 10"
 
+        }
+
+    }
+    
+    
     override func viewDidLoad() {
+  
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
